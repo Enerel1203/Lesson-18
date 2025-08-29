802098d489020681039efbaf0004d724df8d0107
Enerel1203 <enerel.bayanmunkh@ris.ae>
2025-08-29 14:02:10 +0400

    Add files via upload

---
+++ Activity 3.py
@@ -0,0 +1,11 @@
+import math
+
+print('The floor and ceiling value of 23.56 are: ' , math.ceil(23.56) , ',' , math.floor(23.56))
+
+x= 10
+y= -15
+print("The value of x after copying the sign from y is: " , math.copysign(x, y))
+
+print("Absolute value of -96 and 56 are: " , math.fabs(-96) , ',' , math.fabs(56))
+
+print("The GCD of 24 and 56: " , math.gcd(24, 56))

4f1ed80589348178d2ee77c64a80269664439049
Enerel1203 <enerel.bayanmunkh@ris.ae>
2025-09-22 12:00:27 +0400

    Project 17.py

--- Project 17.py
+++ Project 17.py
@@ -1,14 +1,8 @@
 import math
 
-angle = float(input("Enter an angle in degrees: "))
-
+angle = 45   
 radian = math.radians(angle)
 
-sin_val = round(math.sin(radian), 4)
-cos_val = round(math.cos(radian), 4)
-tan_val = "undefined" if cos_val == 0 else round(math.tan(radian), 4)
-
-print(f"\nTrigonometric values for {angle}°:")
-print(f"sin({angle}) = {sin_val}")
-print(f"cos({angle}) = {cos_val}")
-print(f"tan({angle}) = {tan_val}")
+print("sin =", math.sin(radian))
+print("cos =", math.cos(radian))
+print("tan =", math.tan(radian))
